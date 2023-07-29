98ca99b739efecce1294718826b139778ef7b562
Hamideh Parsaeian <hamideh.parsaeian@gmail.com>
2023-07-29 16:15:08 +0330

    Adding GitHub URL in footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      Weather Application
+      <footer>
+        This project is codec by Hamideh Parsaeian and is{" "}
+        <a
+          href="https://github.com/hamidehparsa/weather-application.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
